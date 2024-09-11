f01b320c1f18daba8317be67bbcd61e54f86d918
Mechetel <dima.homa5@gmail.com>
2024-09-12 00:39:45 +0300

    feat: add csvlogger and checkpoint callbacks on_epoch_end

---
+++ callbacks.py
@@ -0,0 +1,19 @@
+import numpy as np
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+
+
+class checkpoint_callback(keras.callbacks.Callback):
+    """
+    Subclass of keras.callbacks.Callback to save the weights every epoch in a .h5 file
+    """
+    def __init__(self, model_path, **kwargs):
+        super(keras.callbacks.Callback, self).__init__(**kwargs)
+        self.model_path = model_path
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.model.save(self.model_path)
